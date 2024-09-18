3ebc3c249a4c73a9fc4ac1eac95acc8ac987d3c2
Alejandro Bernal <jbernalxyz@gmail.com>
2024-09-18 17:20:25 -0500

    fix: added return to map

--- src/Card.tsx
+++ src/Card.tsx
@@ -6,14 +6,11 @@ type Props = {
 };
 
 export const Card: FC<Props> = ({ character }) => {
-  console.log("inside my card");
-  console.log(character);
   return (
     <div>
-      hola mundo
-      <h1>
+      <h2>
         {character.name} and is {character.status}
-      </h1>
+      </h2>
     </div>
   );
 };
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { Character, fetchCharacters } from "./api";
+import { fetchCharacters } from "./api";
 import { Card } from "./Card";
-import { useState } from "react";
 
 export const Home = () => {
   const { status, data, error } = useQuery({
@@ -9,9 +8,6 @@ export const Home = () => {
     queryFn: fetchCharacters,
   });
 
-  const [characters, setCharacters] = useState<Character[]>([]);
-
-  console.log(data);
   if (status === "pending") {
     return <span>Loading...</span>;
   }
@@ -20,18 +16,13 @@ export const Home = () => {
     return <span>Error: {error.message}</span>;
   }
 
-  if (characters.length === 0) {
-    setCharacters(data?.results);
-  }
+  const characters = data?.results;
 
-  console.log("my characters: ", characters);
-
-  // also status === 'success', but "else" logic works, too
   return (
     <>
       <h1>Characters</h1>
-      {characters.map((el, index) => {
-        <Card key={index} character={el} />;
+      {characters.map((ch, index) => {
+        return <Card key={index} character={ch} />;
       })}
     </>
   );
